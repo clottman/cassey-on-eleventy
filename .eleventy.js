012a544d8519484bd3d7691eddf61a11ef8bdbc7
Cassey Lottman <>
2019-08-18 14:45:57 -0500

    add new page for poems

--- .eleventy.js
+++ .eleventy.js
@@ -95,7 +95,7 @@ module.exports = function(eleventyConfig) {
 */
 
   return {
-    templateFormats: ["md", "njk", "html", "liquid"],
+    templateFormats: ["md", "njk", "html", "liquid", "hbs"],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about it.
---
+++ _data/poems.js
@@ -0,0 +1,122 @@
+module.exports = [
+  {
+    name: "Explaining My Depression to My Mother",
+    author: "Sabrina Benaim",
+    url: "https://youtu.be/aqu4ezLQEUA",
+    slowdownUrl: "",
+    video: true
+  },
+  {
+    name: "Shrinking Women",
+    author: "Lily Myers",
+    url: "https://youtu.be/zQucWXWXp3k",
+    video: true
+  },
+  {
+    name: "We Lived Happily During The War",
+    author: "Ilya Kaminsky",
+    url:
+      "https://www.poetryfoundation.org/poems/91413/we-lived-happily-during-the-war",
+    slowdownUrl:
+      "https://www.slowdownshow.org/episode/2019/04/26/110-we-lived-happily-during-the-war"
+  },
+  {
+    name: "A Small Needful Fact",
+    author: "Ross Gay",
+    url: "https://poets.org/poem/small-needful-fact",
+    slowdownUrl:
+      "https://www.slowdownshow.org/episode/2019/07/10/163-a-small-needful-fact"
+  },
+  {
+    name: "Using a Hula Hoop Can Get You Abducted by Aliens",
+    author: "Matthea Harvey",
+    url:
+      "https://www.pbs.org/newshour/arts/weekly-poem-using-a-hula-hoop-can-get-you-abducted-by-aliens",
+    slowdownUrl:
+      "https://www.slowdownshow.org/episode/2019/08/01/179-using-a-hula-hoop-can-get-you-abducted-by-aliens"
+  },
+  {
+    name: "Pocket-Sized Feminism",
+    author: "Blythe Baird",
+    url: "https://youtu.be/vT74LH0W8ig",
+    video: true
+  },
+  {
+    name: "Alcestis on the Poetry Circuit",
+    author: "Erica Jong",
+    url: "https://www.poeticous.com/erica-jong/alcestis-on-the-poetry-circuit"
+  },
+  {
+    name: "Habitation",
+    author: "Margaret Atwood",
+    url: "https://www.poeticous.com/margaret-atwood/habitation"
+  },
+  {
+    name: "Immigrant Can't Write Poetry",
+    author: "Wang Ping",
+    url: "https://poets.org/poem/immigrant-cant-write-poetry",
+    slowdownUrl:
+      "https://www.slowdownshow.org/episode/2019/07/26/175-immigrant-cant-write-poetry"
+  },
+  {
+    name: "Ozymandias",
+    author: "Percy Bysshe Shelley",
+    url: "https://www.poetryfoundation.org/poems/46565/ozymandias",
+    slowdownUrl:
+      "https://www.slowdownshow.org/episode/2019/06/11/142-ozymandias"
+  },
+  {
+    name: "Indigo",
+    author: "Ellen Bass",
+    url: "https://poets.org/poem/indigo",
+    slowdownUrl: ""
+  },
+  {
+    name: "If I Was Your God",
+    author: "Dave McAlinden",
+    url: "https://youtu.be/LZhkZoG4vOs",
+    video: true
+  },
+  {
+    name: "White Heaven",
+    author: "Jahman Hill",
+    url: "https://youtu.be/F-UYSus32bA",
+    video: true
+  },
+  {
+    name: "Sum of Her Parts",
+    author: "Desireé Dallagiacomo",
+    url: "https://youtu.be/9H8B6JtHheQ",
+    video: true
+  },
+  {
+    name: "Black Boy Auditions For His Own Funeral",
+    author: "Gabriel Ramirez",
+    url: "https://youtu.be/ZBnQbEUKfrs",
+    video: true
+  },
+  {
+    name: "This Woman",
+    author: "Alysia Harris",
+    url: "https://youtu.be/5fUvEnocXKY",
+    video: true
+  },
+  {
+    name: "Table Games",
+    author: "Sarah Kay",
+    url: "https://youtu.be/tlcTGDRX450",
+    video: true
+  },
+  {
+    name: "The Type",
+    author: "Sarah Kay",
+    url: "https://youtu.be/qYAiYMlOCI4",
+    video: true
+  },
+  {
+    name: "Some Things You Need to Know Before Dating Me",
+    author: "Jamie Mortara",
+    url: "https://youtu.be/-BaO3iU2ICA",
+    video: true
+  }
+];
