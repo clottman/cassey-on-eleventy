19cff9e69ef59f7f119a5170ae04d106e957e011
Cassey Lottman <>
2020-03-15 23:40:05 -0500

    til pages are working except for tags

--- _data/til.js
+++ _data/til.js
@@ -11,16 +11,18 @@ module.exports = () => {
                 // turn the feed XML into JSON
                 toJSON(response.data, function (err, result) {
                     // create a path for each item based on Medium's guid URL
-                    console.log(result.feed.entry[1].tags[0].value);
                     result.feed.entry.forEach(element => {
                         var url = element.id[0].split("/");
                         // -2 because last element was a slash, so the last element of the split will be ""
                         element.url = `/til/${url[url.length - 2]}`;
                         element.content = element.content[0]["_"];
+                        const tags = element.tags[0].value.filter(tag => {
+                            return tag !== 'post' && tag !== 'posts';
+                        });
                         element.data = {
                             date: element.updated[0],
                             title: element.title,
-                            tags: element.tags[0].value,
+                            tags: tags,
                         };
                     });
                     resolve({ url: url, posts: result.feed.entry });
