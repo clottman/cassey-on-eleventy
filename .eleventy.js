2670ef27d46e13b639c086b321e5577575c8b946
Cassey Lottman <clottman@users.noreply.github.com>
2020-10-31 11:53:03 -0500

    regular blog tags work

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,53 @@ const fs = require("fs");
 const path = require("path");
 const getTagList = require("./_11ty/getTagList");
 const MarkdownIt = require("markdown-it");
+const lodash = require("lodash");
+const slugify = require("slugify");
+
+/**
+ * Get all unique key values from a collection
+ *
+ * @param {Array} collectionArray - collection to loop through
+ * @param {String} key - key to get values from
+ * https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/
+ */
+function getAllKeyValues(collectionArray, key) {
+  // get all values from collection
+  let allValues = collectionArray.map((item) => {
+    let values = item.data[key] ? item.data[key] : [];
+    return values;
+  });
+
+  // flatten values array
+  allValues = lodash.flattenDeep(allValues);
+  // to lowercase
+  allValues = allValues.map((item) => item.toLowerCase());
+  // remove duplicates
+  allValues = [...new Set(allValues)];
+  // order alphabetically
+  allValues = allValues.sort(function (a, b) {
+    return a.localeCompare(b, "en", { sensitivity: "base" });
+  });
+  // return
+  return allValues;
+}
+
+/**
+ * Transform a string into a slug
+ * Uses slugify package
+ *
+ * @param {String} str - string to slugify
+ * https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/
+ */
+function strToSlug(str) {
+  const options = {
+    replacement: "-",
+    remove: /[&,+()$~%.'":*?<>{}]/g,
+    lower: true,
+  };
+
+  return slugify(str, options);
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
@@ -78,11 +125,29 @@ module.exports = function (eleventyConfig) {
         else return 0;
       })
   );
-
+  const tilGlob = "til/*.*";
   eleventyConfig.addCollection("til", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("til/*.*");
+    return collectionApi.getFilteredByGlob(tilGlob);
   });
 
+  // create til-tags collection
+  eleventyConfig.addCollection("tilTags", function (collection) {
+    let allTilTags = getAllKeyValues(
+      collection.getFilteredByGlob(tilGlob),
+      "til-tags"
+    );
+    console.log(allTilTags);
+
+    let tilTags = allTilTags.map((tilTag) => ({
+      title: tilTag,
+      slug: strToSlug(tilTag),
+    }));
+    console.log(tilTags);
+
+    return tilTags;
+  });
+
+  eleventyConfig.addFilter("include", require("./filters/include.js"));
   /*
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
---
+++ filters/include.js
@@ -0,0 +1,25 @@
+// from: https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/ 
+
+const lodash = require("lodash");
+
+/**
+ * Select all objects in an array
+ * where the path includes the value to match
+ * capitalisation and diacritics are removed from compared values
+ *
+ * @param {Array} arr - array of objects to inspect
+ * @param {String} path - path to inspect for each object
+ * @param {String} value - value to match
+ * @return {Array} - new array
+ */
+
+module.exports = function (arr, path, value) {
+  value = lodash.deburr(value).toLowerCase();
+  const filtered = arr.filter((item) => {
+    let pathValue = lodash.get(item, path);
+    pathValue = lodash.deburr(pathValue).toLowerCase();
+    return pathValue.includes(value);
+  });
+ // console.log(filtered);
+  return filtered;
+};
