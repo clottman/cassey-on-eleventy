cb02ac675bac5e374bcc1ebb2753a97a5010231d
Cassey Lottman <clottman@users.noreply.github.com>
2022-05-30 12:53:17 -0500

    switch to png

--- imageHelpers.js
+++ imageHelpers.js
@@ -1,14 +1,14 @@
 const Image = require("@11ty/eleventy-img");
 
 // get just the output url for an image, for use in social cards
-async function imgUrlShortcode(src, widths = [300], formats = ["jpeg"]) {
+async function imgUrlShortcode(src, widths = [300]) {
     let metadata = await Image(src, {
       widths: widths,
-      formats: formats,
+      formats: ["png"],
       outputDir: './_site/img/'
     });
   
-    let data = metadata.jpeg[metadata.jpeg.length - 1];
+    let data = metadata.png[metadata.png.length - 1];
     return data.url;
   }
 
